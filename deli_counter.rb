822eb08c052db49468f449cdcff93adef013cad7
Cory Marker <cmarker4@gmail.com>
2020-07-18 20:08:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli = []
+
+def line(arr)
+  line = []
+  if arr.size < 1
+    puts "The line is currently empty."
+  else
+    arr.each_with_index(1) do |name, index|
+      line.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{line.join(" ")}"
+  end
+end
