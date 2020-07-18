ad76e2734b05d57b83d1142835b75e3ed69c0e93
Cory Marker <cmarker4@gmail.com>
2020-07-18 20:12:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,8 @@ def line(arr)
     puts "The line is currently: #{line.join(" ")}"
   end
 end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
